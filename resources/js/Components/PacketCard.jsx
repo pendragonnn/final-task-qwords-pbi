194224e77c357f1ceb790565a285950a2ae4293b
Wisnu Andika <wisnuandika104@gmail.com>
2023-12-21 12:48:47 +0700

    fix: fix overflow text at packet card component

--- resources/js/Components/PacketCard.jsx
+++ resources/js/Components/PacketCard.jsx
@@ -5,7 +5,7 @@ export default function PacketCard({ packetData }) {
   return (
     <div className='flex items-center justify-center'>
       <Card
-        className="w-full h-[400px] bg-orange-500"
+        className="w-full h-full bg-orange-500"
         imgAlt="Meaningful alt text for an image that is not purely decorative"
         imgSrc={packetData.img}
       >
