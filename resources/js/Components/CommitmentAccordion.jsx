f413ce51af2c751e230d7f6f65f864bf800d6bda
Wisnu Andika <wisnuandika104@gmail.com>
2023-12-24 14:18:06 +0700

    feat: add logo in commitment accordion component

--- resources/js/Components/CommitmentAccordion.jsx
+++ resources/js/Components/CommitmentAccordion.jsx
@@ -4,7 +4,14 @@ export default function CommitmentAccordion({ commitmentData }) {
   return (
     <Accordion collapseAll className='my-1'>
       <Accordion.Panel>
-        <Accordion.Title className='bg-orange-500 text-white hover:bg-orange-500'>{commitmentData.title}</Accordion.Title>
+        <Accordion.Title className='bg-orange-500 hover:bg-orange-500 text-white'>
+          <div className='flex items-center gap-5'>
+            <div className='p-2 bg-white rounded-full'>
+              <img className='w-[32px]' src={commitmentData.logo} alt="" />
+            </div>
+            <p>{commitmentData.title}</p>
+          </div>
+        </Accordion.Title>
         <Accordion.Content >
           <p>{commitmentData.description}</p>
         </Accordion.Content>
